c0bb28a8f95f18509090f940a7431bc8473c2535
Agustin Alexander <aalexander@campus.ungs.edu.ar>
2019-07-30 14:20:24 -0300

    changed filename

--- web/index.php
+++ web/index.php
@@ -1,20 +1,27 @@
 <?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP File Upload</title>
+</head>
+<body>
+  <?php
+    if (isset($_SESSION['message']) && $_SESSION['message'])
+    {
+      printf('<b>%s</b>', $_SESSION['message']);
+      unset($_SESSION['message']);
+    }
+  ?>
+  <form method="POST" action="upload.php" enctype="multipart/form-data">
+    <div>
+      <span>Upload a File:</span>
+      <input type="file" name="uploadedFile" />
+    </div>
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return;
-});
-
-$app->run();
+    <input type="submit" name="uploadBtn" value="Upload" />
+    <a href="download.php?file=output.pdf">Download file</a>
+  </form>
+</body>
+</html>
