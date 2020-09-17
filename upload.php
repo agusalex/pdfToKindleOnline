f5ca45120ee6ddf05076252ed6e681203e76e9af
Agustin Alexander <aalexander@campus.ungs.edu.ar>
2020-09-17 00:01:45 -0300

    minor change

--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@ if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
 
       if(move_uploaded_file($fileTmpPath, $dest_path)) 
       {
-        $message = shell_exec('echo -ne "\n" | k2pdfopt -dev pw3 '. $dest_path . ' -o output.pdf >> log.log');
+        $message = shell_exec('echo -ne "\n" | ./k2pdfopt -dev pw3 '. $dest_path . ' -o output.pdf >> log.log');
         $message = "file converted!";
         $_SESSION['message'] = $message;
       }
